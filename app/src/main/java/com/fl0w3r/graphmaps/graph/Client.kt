8bae0f259482040313ae060cd1ae709879785110
p0ppy-fl0w3r <kunai_madara@protonmail.com>
2023-01-09 10:29:04 +0545

    Add graphQL client.

---
+++ app/src/main/java/com/fl0w3r/graphmaps/graph/Client.kt
@@ -0,0 +1,29 @@
+package com.fl0w3r.graphmaps.graph
+
+import android.content.Context
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.network.okHttpClient
+import okhttp3.OkHttpClient
+
+
+
+object GraphMapClient{
+    private lateinit var INSTANCE: ApolloClient
+
+    fun apolloClient(): ApolloClient {
+        if (::INSTANCE.isInitialized) {
+            // Return the client instance if it's been initialized already.
+            return INSTANCE
+        }
+
+        val okHttpClient = OkHttpClient.Builder()
+            .build()
+
+        INSTANCE = ApolloClient.Builder()
+            .serverUrl("https://graphqlzero.almansi.me/api")
+            .okHttpClient(okHttpClient)
+            .build()
+
+        return INSTANCE
+    }
+}
